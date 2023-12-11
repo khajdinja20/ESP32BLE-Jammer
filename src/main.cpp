e295b1749d8985c68b5eeee49ff66a03d9385958
Karlo Hajdinjak <khajdinja20@foi.hr>
2023-12-11 15:10:49 +0100

    Made the device portable

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,50 @@ RF24 radio(CE_PIN_NRF1, CSN_PIN_NRF1); // CE, CSN
 RF24 radio2(CE_PIN_NRF2, CSN_PIN_NRF2);
 RF24 radio3(CE_PIN_NRF3, CSN_PIN_NRF3);
 
+// Needed for portability
+const int ledPin = 22;
+const int buttonPin = 27;
+
+RTC_DATA_ATTR int bootCount = 0;
+
+int ledState = LOW;
+int buttonState = LOW;
+int lastButtonState;
+
+void print_wakeup_reason()
+{
+    esp_sleep_wakeup_cause_t wakeup_reason;
+
+    wakeup_reason = esp_sleep_get_wakeup_cause();
+
+    switch (wakeup_reason)
+    {
+    case ESP_SLEEP_WAKEUP_EXT0:
+        Serial.println("Wakeup caused by external signal using RTC_IO");
+        break;
+    case ESP_SLEEP_WAKEUP_EXT1:
+        Serial.println("Wakeup caused by external signal using RTC_CNTL");
+        break;
+    case ESP_SLEEP_WAKEUP_TIMER:
+        Serial.println("Wakeup caused by timer");
+        break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD:
+        Serial.println("Wakeup caused by touchpad");
+        break;
+    case ESP_SLEEP_WAKEUP_ULP:
+        Serial.println("Wakeup caused by ULP program");
+        break;
+    default:
+        Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+        break;
+    }
+}
+
 void setup()
 {
     Serial.begin(115200);
+    delay(500);
+
     Serial.println("Setup starting!");
     radio.begin();
     radio2.begin();
@@ -59,11 +100,38 @@ void setup()
     radio3.setChannel(2);
     delay(1000);
 
+    Serial.println("Portability pack setup start");
+    pinMode(buttonPin, INPUT_PULLUP);
+    pinMode(ledPin, OUTPUT);
+
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+
     Serial.println("Setup finished.");
+
+    ++bootCount;
+    Serial.println("Boot number: " + String(bootCount));
+    print_wakeup_reason();
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_27, HIGH);
+
+    digitalWrite(ledPin, HIGH);
 }
 
 void loop()
 {
+    lastButtonState = buttonState;
+    buttonState = digitalRead(buttonPin);
+    if (lastButtonState == HIGH && buttonState == LOW)
+    {
+        Serial.println("Button pressed");
+        Serial.print("Led state:");
+        Serial.println(ledState);
+        ledState = !ledState;
+        digitalWrite(ledPin, ledState);
+        Serial.println("Going to sleep");
+        esp_deep_sleep_start();
+    }
     byte text = 255; // just some random string
     radio.writeFast(&text, sizeof(text));
     radio2.writeFast(&text, sizeof(text));
